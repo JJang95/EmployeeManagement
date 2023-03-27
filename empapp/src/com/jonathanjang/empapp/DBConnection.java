68f22083b185ee3f371dd30f26dba3a5070ff361
JJang95 <jojang1994@gmail.com>
2023-03-27 14:30:23 -0400

    Finished creating connection class

--- empapp/src/com/jonathanjang/empapp/DBConnection.java
+++ empapp/src/com/jonathanjang/empapp/DBConnection.java
@@ -1,5 +1,22 @@
 package com.jonathanjang.empapp;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBConnection {
+    static Connection con;
+    public static Connection createDBConnection() {
+            try {
+                //load driver
+                Class.forName("com.mysql.jdbc.Driver");
+                //get connection
+                String url = "jdbc:mysql://localhost:3306/employeeDB";
+                String username = "root";
+                String password = "root_password_1234";
+                con = DriverManager.getConnection(url,username,password);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        return con;
+    }
 }
